9736adad758d95fc009eaa7fcccdbcb9cc68dd01
SomeHandyGuy <some.guy@optusnet.com.au>
2017-05-29 14:03:54 +1000

    Revert "Question bank added"
    This reverts commit b1fb509eebd2667039fc1bf101e4b9fe3de74a26.

--- QuestionBank/16.js
+++
@@ -1,8 +0,0 @@
-function makeVars16() {
-    var qNum = {p: randomInt(1, 99), q: randomInt(10, 1000), "$": "`"};
-    qNum['dp'] = qNum['p'] / 100;
-    qNum['a'] = qNum['q'] * qNum['p'] / 100;
-    qNum['question'] = '<p>Find [p]% of [q]</p>';
-    qNum['answer'] = '<p><b>Using Decimals:</b></p><p>[$][q]xx[dp]=[a][$]</p>';
-    setup(16, qNum);
-}makeVars16();
--- QuestionBank/40.js
+++
@@ -1,8 +0,0 @@
-function makeVars40() {
-    var qNum = {'x1': randomInt(-10, 15), 'y1': randomInt(-10, 15), 'x2': randomInt(-10, 15), 'y2': randomInt(-10, 15), "$": "`"};
-    var m = new Fraction(qNum['y2'] - qNum['y1'], qNum['x2'] - qNum['x1']);
-    qNum['m'] = m.string();
-    qNum['question'] = '<p>What is the gradient of the line passing through the points [$]([x1],[y1])[$] and [$]([x2],[y2])[$]?</p>';
-    qNum['answer'] = '<p>Using the gradient formula:</p><p>[$]m=(y_2-y_1)/(x_2-x_1)[$]</p><p>We have:</p><p>[$]m=([y2]- [y1])/([x2]- [x1])=[m][$]</p>';
-    setup(40, qNum);
-}makeVars40();
